89734b3d714993c392a3f4e691612fd3968d0691
Tusnelda91 <julin@gmx.net>
2017-06-28 12:39:09 +0200

    added price field to products

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,8 +6,13 @@ end
 
 class StaticPagesController < ApplicationController
 	
+
+  #def landing_page
+  #	@featured_product = Product.first
+  #end  
+
   def landing_page
-  	@featured_product = Product.first
+  @products = Product.limit(3)
   end
   
 end
