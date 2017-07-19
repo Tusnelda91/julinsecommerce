2ea0423566a29700998390ed444388cbad013831
Tusnelda91 <julin@gmx.net>
2017-07-19 13:40:09 +0200

    added comment model, relationships and scopes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,21 @@
 class Product < ApplicationRecord
+	has_many :comments
 	
   def self.search(search_term)
     like_operator = Rails.env.production? ? 'ilike' : 'like'
     Product.where("name #{like_operator} ?", "%#{search_term}%")
   end
-	#def self.search(search_term)
-     #   Product.where("name LIKE ?", "%#{search_term}%")
-    #end
+	
+  def highest_rating_comment
+	comments.rating_desc.first
+  end
+
+  def lowest_rating_comment 
+    comments.rating_asc.first
+  end
+
+  def average_rating
+    comments.average(:rating).to_f
+  end
+
 end
