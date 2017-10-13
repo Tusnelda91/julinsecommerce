b1eee45b7d8b7213bac0362a7fc91de4b7df9e22
Tusnelda91 <julin@gmx.net>
2017-10-13 13:53:45 +0200

    changed navbar and spec-files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,11 +64,13 @@ class UsersController < ApplicationController
     end
   end
 
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
     end
+
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
